5807705937e3391c97cec5e885e165ed10e1999d
Oren Berkowitz <oren.berkowitz@soundcloud.com>
2021-02-19 21:09:20 +0100

    Have both icecast and rtmp solutions live side by side

---
+++ streaming-commands.sh
@@ -0,0 +1,9 @@
+
+# 64k opus in an ogg (seems to not be supported by iPhone)
+
+ffmpeg -re -i $INPUT_FILE -c:a libopus -vbr on -b:a 64k -content_type 'audio/ogg' -vn -f opus icecast://source:A8A3DD93C15E@localhost:8000/silent-disco.ogg
+
+# 128k mp3 via lame (works natively on iPhone)
+
+ffmpeg -re -i $INPUT_FILE -c:a libmp3lame -b:a 128k -content_type 'audio/mpeg' -vn -f mp3 icecast://source:A8A3DD93C15E@localhost:8000/silent-disco.mp3
+
